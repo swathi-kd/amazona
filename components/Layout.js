1c172dbd7ee9655225a0204d3195ffe4d0a7733a
Swathi K D <swathikd09@gmail.com>
2022-04-01 14:17:28 +0530

    Product listing page and product description page

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,43 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 import Head from 'next/head';
+import NextLink from 'next/link';
 import React from 'react';
+import useStyles from '../utils/styles';
 
-export default function Layout({ children }) {
+export default function Layout({ title, description, children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
-        <title>Amazona</title>
+        <title>{title ? `${title} - Amazona` : 'Amazona'}</title>
+        {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>amazona</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All rights reserved</Typography>
       </footer>
     </div>
